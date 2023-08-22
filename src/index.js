0411091180ee51e23747383207577c5f8b661143
codehyouka <plants.coordinators@gmail.com>
2023-08-23 00:17:31 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const one =1;
  * @returns {any} Return the boolean.
  * @example
  *
- * data = urlComposer('https://example.com')
+ * data = urlComposer('https://example.com');
  * data.getToString()
  *=> 'https://example.com'
  */
